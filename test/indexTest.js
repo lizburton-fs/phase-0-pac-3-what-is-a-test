a0d85357bd6ba3a31ee65c6681b29eaef720f6f5
lizburton-fs <115805169+lizburton-fs@users.noreply.github.com>
2022-12-22 16:37:08 -0500

    Update indexTest.js

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,7 +2,31 @@ require ( './root.js' );
 
 const { name, height, message } = require("../index.js");
 
+/*
 describe('what-is-a-test', () => {
-  return true;
+  describe('Name', () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual('Susan')
+    })
+  })
+  describe('Height', () => {
+    it('is less than 40', () => {
+      expect(height).toBeLessThan(40)
+    })
+  })
+  describe('Message', () => {
+    it('gives the name and height', () => {
+      expect(message).toInclude(name)
+      expect(message).toInclude(height)
+    })
+  })
 })
+*/
 
+describe('what-is-a-test', () => {
+  describe('Name', () => {
+    it('exists', () => {
+      expect(name).to.exist;
+    })
+  })
+})
